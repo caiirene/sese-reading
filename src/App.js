be47f6ac6a0e420c5862dda3b3423e14502b63a2
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-12-04 17:42:13 -0800

    update users

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes, 
-  Route,
-  Link
-} from 'react-router-dom';
-import './App.css';
-import Home from './Containers/Home/Home';
-import BookCard from './Components/BookCard/Bookcard';
-import BookShowcase from './Components/BookShowcase/BookShowcase';
-import AuthorZone from './Components/AuthorZone/AuthorZone';
-import Myworks from './Components/Myworks/Myworks';
-import CreateWork from './Components/CreateWork/CreateWork';
-import BookShowcaseAdministrators from './Components/BookShowcaseAdministrators/BookShowcaseAdministrators';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./Containers/Home/Home";
+import BookCard from "./Components/BookCard/Bookcard";
+import BookShowcase from "./Components/BookShowcase/BookShowcase";
+import AuthorZone from "./Components/AuthorZone/AuthorZone";
+import Myworks from "./Components/Myworks/Myworks";
+import CreateWork from "./Components/CreateWork/CreateWork";
+import BookShowcaseAdministrators from "./Components/BookShowcaseAdministrators/BookShowcaseAdministrators";
+import Signin from "./Components/users/signin";
+import Signup from "./Components/users/signup";
+import Account from "./Components/users/account";
+import UserTable from "./Components/users/table";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <Link to="/book">BookCard</Link>
-          <Link to="/authorzone">AuthorZone</Link>
-          <Link to="/bookshowcase">BookShowcase</Link>
-          <Link to="/myworks">Myworks</Link>
-          <Link to="/creatework">CreateWork</Link>
-          <Link to="/bookshowcaseadministrators">BookShowcaseAdministrators</Link>
-        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/book" element={<BookCard />} />
@@ -33,7 +24,16 @@ function App() {
           <Route path="/myworks" element={<Myworks />} />
           <Route path="/creatework" element={<CreateWork />} />
           <Route path="/bookshowcase" element={<BookShowcase />} />
-          <Route path="/bookshowcaseadministrators" element={<BookShowcaseAdministrators />} />
+          <Route
+            path="/bookshowcaseadministrators"
+            element={<BookShowcaseAdministrators />}
+          />
+
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/account/:id" element={<Account />} />
+          <Route path="/admin/users" element={<UserTable />} />
         </Routes>
       </div>
     </Router>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+function NavBar() {
+  return (
+    <nav>
+      {/* Other navigation links */}
+      <Link to="/book">BookCard</Link>
+      <Link to="/authorzone">AuthorZone</Link>
+      <Link to="/bookshowcase">BookShowcase</Link>
+      <Link to="/myworks">Myworks</Link>
+      <Link to="/creatework">CreateWork</Link>
+      <Link to="/bookshowcaseadministrators">BookShowcaseAdministrators</Link>
 
-const NavBar = () => (
-  <nav>
-    {/* Other navigation links */}
-    <Link to="/book">BookCard</Link>
-  </nav>
-);
+      {/* User navigation links */}
+      <Link to="/signin">signin</Link>
+      <Link to="/signup">signup</Link>
+
+      
+    </nav>
+  );
+}
 
 export default NavBar;
