7598e800ca5772ca32fe0c30a0f0cc45bad8e379
HUIYING YU <87959635+yuyh9@users.noreply.github.com>
2023-12-09 01:38:31 -0800

    update bookcard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { useEffect, useState } from "react";
 import Home from "./Containers/Home/Home";
 import BookCard from "./Components/BookCard/Bookcard";
 import BookShowcase from "./Components/BookShowcase/BookShowcase";
@@ -16,7 +17,10 @@ import NavBar from "./Components/NavBar/NavBar";
 import Search from "./Components/Search/search";
 import Detail from "./Components/Search/detail";
 import store from "../src/Components/books/store";
+import BookList from "./Components/books/bookList";
 import { Provider } from "react-redux";
+import Profile from "./Components/Account/profile";
+import * as client from "../src/Components/books/client";
 
 function App() {
   return (
@@ -26,7 +30,9 @@ function App() {
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/book" element={<BookCard />} />
+            <Route path="/books" element={<BookList />} />
+            <Route path="/book/:bookId" element={<BookCard />} />
+
             <Route path="/authorzone" element={<AuthorZone />} />
             <Route path="/myworks" element={<Myworks />} />
             <Route path="/creatework" element={<CreateWork />} />
@@ -45,6 +51,7 @@ function App() {
             <Route path="/detail/:bookId" element={<Detail />} />
 
             <Route path="/search" element={<Search />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
       </Router>
