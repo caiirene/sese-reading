9e2f6de53c2260952cc4f03ed7832d5215316d0a
caiirene <aaqqii901@gmail.com>
2023-12-09 15:41:47 -0800

    newBranch

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Home.css'; // Make sure the path is correct based on your project structure
 
 // Import any other components you want to use on the Home page
-import NavBar from '/Users/Andrea/Desktop/sese-reading-main/src/Components/NavBar/NavBar.js';
+import NavBar from '../../Components/NavBar/NavBar';
+
 
 function Home() {
 
