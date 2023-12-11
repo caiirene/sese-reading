cf0acefc85feb063af65a95072296308306c7553
Michelle-MingxuanWang <mingxuan.wang@outlook.com>
2023-12-10 23:15:25 -0800

    Add Profile comment section and edit home page

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import BookList from "../../Components/books/bookList";
+import CommentsList from "../../Components/Comments/commentslist";
 function Home() {
   return (
     <div className="home">
@@ -13,6 +14,8 @@ function Home() {
         <h2>Book List</h2>
       </section>
       <BookList />
+      <h1 className="text-center">My Recent Reviews</h1>
+      <CommentsList />
     </div>
   );
 }
