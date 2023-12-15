dc33e9eb9d9cbe5112db7277d4c2ea820bf3b097
caiirene <aaqqii901@gmail.com>
2023-12-14 22:35:48 -0800

    logo size

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -90,7 +90,7 @@ function NavBar() {
     <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#e3f2fd", fontFamily: "'Open Sans', sans-serif" }}>
       <div className="container-fluid">
 
-      <img src={logo} />
+      <img src={logo} width="45" height="45"/>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
