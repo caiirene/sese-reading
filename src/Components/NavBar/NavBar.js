308c94a540058fa054236cc8b8fb6cc1db47e4c3
caiirene <aaqqii901@gmail.com>
2023-12-14 22:32:26 -0800

    ./

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import Logo from 'WebLogo.png';
+import Logo from "./WebLogo.png";
 
 function NavBar() {
 
